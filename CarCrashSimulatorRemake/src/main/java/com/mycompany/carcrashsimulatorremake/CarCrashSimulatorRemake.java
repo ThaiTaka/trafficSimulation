0b40a7614e7f610c1c65b66fdb0ed268177f7a0f
Thái Taka <144147780+ThaiTaka@users.noreply.github.com>
2025-03-14 18:36:50 +0700

    Add files via upload

---
+++ CarCrashSimulatorRemake/src/main/java/com/mycompany/carcrashsimulatorremake/CarCrashSimulatorRemake.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carcrashsimulatorremake;
+
+/**
+ *
+ * @author karin
+ */
+public class CarCrashSimulatorRemake {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
